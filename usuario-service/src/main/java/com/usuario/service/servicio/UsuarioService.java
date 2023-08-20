66eaf1de74608960915d17f6942f4bf0f0dc3049
Ronald Melara <ronald.melara@gmail.com>
2023-08-20 13:06:20 -0400

    Se incorpora RestTemplate para iniciar integracòn de microservicios

--- usuario-service/src/main/java/com/usuario/service/servicio/UsuarioService.java
+++ usuario-service/src/main/java/com/usuario/service/servicio/UsuarioService.java
@@ -4,26 +4,35 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import com.usuario.service.entidades.Usuario;
+import com.usuario.service.modelos.Auto;
 import com.usuario.service.repositorio.UsuarioRepository;
 
 @Service
 public class UsuarioService {
 	@Autowired
+	private RestTemplate restTemplate;
+
+	@Autowired
 	private UsuarioRepository usuarioRepository;
-	
-	public List<Usuario> getAll(){
+
+	public List<Auto> getAutos(int usuarioId) {
+		List<Auto> autos = restTemplate.getForObject("http://localhost:8002/auto/usuario/" + usuarioId, List.class);
+		return autos;
+	}
+
+	public List<Usuario> getAll() {
 		return usuarioRepository.findAll();
 	}
-	
+
 	public Usuario getUsuarioById(int id) {
 		return usuarioRepository.findById(id).orElse(null);
 	}
-	
+
 	public Usuario save(Usuario usuario) {
 		Usuario nuevoUsuario = usuarioRepository.save(usuario);
 		return nuevoUsuario;
 	}
 }
-	
